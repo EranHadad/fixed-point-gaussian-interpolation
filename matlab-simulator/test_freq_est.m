eb55a14ae6ac32779467a623eb2d4f385cf2f1a6
Eran Hadad <eran.hdd@gmail.com>
2020-06-23 18:27:24 +0300

    matlab-simulator: adding script for FFT frequency estimation using parabolic and Gaussian interpolation

---
+++ matlab-simulator/test_freq_est.m
@@ -0,0 +1,61 @@
+clear; close; clc;
+
+% Script parameters:
+% ------------------
+N = 1024;
+wintype = 'hamming'; % 'hamming' % 'rectwin'
+log_func = @log2; % @log2 % @log
+k_target = 20.5;
+% -----------------------------------------------
+
+% generate signal
+nfft = N;
+w = 2*pi*k_target/nfft;
+nn = 0:(N-1);
+nn = nn(:); % convert to column vector
+x = sin(w .* nn);
+
+% apply window
+switch wintype
+    case 'hamming'
+        x = x .* hamming(N);
+    otherwise
+        x = x .* rectwin(N);
+end
+
+% fourier transform (FFT)
+xfft = fft(x, nfft);
+xfft = abs(xfft(1:(nfft/2+1)));
+
+% display fourier transform
+figure;
+ax1 = subplot(2,1,1);
+plot(x);
+title('signal in time domain');
+xlabel('sample index');
+ax2 = subplot(2,1,2);
+plot(xfft);
+title('signal in frequency domain');
+xlabel('bin index');
+
+% get peak info
+[max_val, max_ind] = max(xfft);
+amp_center = max_val;
+amp_left = xfft(max_ind - 1);
+amp_right = xfft(max_ind + 1);
+k_raw_est = max_ind - 1;
+fprintf('true bin index: %f\n', k_target);
+fprintf('bin index estimation without interplolation: %f\n', k_raw_est);
+
+% parabolic interpolation
+bin_update_par = 0.5 * (amp_right - amp_left) / (2*amp_center - amp_right - amp_left);
+k_parabolic_est = k_raw_est + bin_update_par;
+fprintf('bin index estimation using parabolic interplolation: %f\n', k_parabolic_est);
+
+% gaussian interpolation
+amp_center = log_func(amp_center);
+amp_left = log_func(amp_left);
+amp_right = log_func(amp_right);
+bin_update_gau = 0.5 * (amp_right - amp_left) / (2*amp_center - amp_right - amp_left);
+k_gaussian_est = k_raw_est + bin_update_gau;
+fprintf('bin index estimation using gaussian interplolation: %f\n', k_gaussian_est);
