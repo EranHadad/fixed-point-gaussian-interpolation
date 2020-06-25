cbbf9f803278dd50dd41c7465f182130b4aa0a5d
Eran Hadad <eran.hdd@gmail.com>
2020-06-25 09:41:23 +0300

    matlab-simulator: adding analysis for c fixed-point log function

--- matlab-simulator/call_dll_function.m
+++ matlab-simulator/call_dll_function.m
@@ -1,3 +1,10 @@
+% Script parameters:
+% ------------------
+func_name = 'fxlog_mod'; % 'fxlog' % 'fxlog_mod'
+n_fraction_bits = 7; % 7 % 16
+xx = logspace(-6, 6, 100);
+% -----------------------------------------------
+
 % Load the library.
 if ~libisloaded('FixedPointLib')
     dll_path = [pwd '\FixedPointLib\x64\Debug\FixedPointLib.dll'];
@@ -6,20 +13,64 @@ if ~libisloaded('FixedPointLib')
 end
 
 % Display function signature.
-libfunctionsview('FixedPointLib')
+% libfunctionsview('FixedPointLib')
 
 %% Call the function.
-float_in = exp(5);
-n_fraction_bits = 16; % 16 % 7
-if calllib('FixedPointLib', 'is_positive_representable', float_in, n_fraction_bits)
-    fxp_in = calllib('FixedPointLib', 'float_to_q', float_in, n_fraction_bits)
-    fxp_out = calllib('FixedPointLib', 'fxlog', fxp_in) % fxlog % fxlog_mod
-    float_out = calllib('FixedPointLib', 'q_to_float', fxp_out, n_fraction_bits)
-else
-    fprintf('x = %.4f can NOT be represent by Q%d fixed-point number', x, n_fraction_bits);
-    x = nan;
+yy = nan(size(xx));
+for n = 1:length(xx)
+    float_in = xx(n);
+    if calllib('FixedPointLib', 'is_positive_representable', float_in, n_fraction_bits)
+        fxp_in = calllib('FixedPointLib', 'float_to_q', float_in, n_fraction_bits);
+        fxp_out = calllib('FixedPointLib', func_name, fxp_in);
+        float_out = calllib('FixedPointLib', 'q_to_float', fxp_out, n_fraction_bits);
+        yy(n) = float_out;
+    end
 end
 
+%% Display results
+matlab_log = log(xx);
+error = yy - matlab_log;
+relative_error = 100 * abs(error)./matlab_log;
+
+titlestr = sprintf('log(x) implemented for Q%d', n_fraction_bits);
+
+figure('name','fixed-point log(x)');
+ax1 = subplot(2,1,1);
+semilogx(xx, yy, 'color', 'b');
+hold on; semilogx(xx, yy, '.', 'color', 'b');
+hold on; semilogx(xx, matlab_log, '--'); hold off;
+title(titlestr);
+xlabel('x', 'fontsize', 16);
+ylabel('log(x)', 'fontsize', 16);
+grid on;
+
+ax2 = subplot(2,1,2);
+semilogx(xx, error, 'color', 'b');
+hold on; semilogx(xx, error, '.', 'color', 'b');
+xlabel('x', 'fontsize', 16);
+ylabel('error(x)', 'fontsize', 16);
+grid on;
+
+linkaxes([ax1, ax2],'x');
+
+figure('name','fixed-point log(x)');
+ax1 = subplot(2,1,1);
+semilogx(xx, yy, 'color', 'b');
+hold on; semilogx(xx, yy, '.', 'color', 'b');
+hold on; semilogx(xx, matlab_log, '--'); hold off;
+title(titlestr);
+xlabel('x', 'fontsize', 16);
+ylabel('log(x)', 'fontsize', 16);
+grid on;
+
+ax2 = subplot(2,1,2);
+semilogx(xx, relative_error, 'color', 'b');
+hold on; semilogx(xx, relative_error, '.', 'color', 'b');
+xlabel('x', 'fontsize', 16);
+ylabel('relative error(x) (%)', 'fontsize', 16);
+grid on;
+
+linkaxes([ax1, ax2],'x');
 
 %% Cleanup.
 unloadlibrary('FixedPointLib');
